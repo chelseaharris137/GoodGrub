4b9ad228abd44d157f85df310887075ad00caa73
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-01-20 13:28:37 -0500

    Add react query and query client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,14 @@
+import { QueryClient } from 'react-query'
 import '../styles/globals.css'
+
+const queryClient = new QueryClient({
+  defaultOptions : {
+    queries: {
+      cacheTime: 1 * 60 * 60 * 1000,
+      staleTime: 1 * 60 * 60 * 1000,
+    }
+  }
+})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
