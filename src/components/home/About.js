59c402985e21c858784507749e812db70f0d91ef
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-01-25 12:12:56 -0500

    Add about component

---
+++ src/components/home/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, Title } from '../text';
+import classes from './About.module.scss';
+
+export const About = () => {
+  return (
+    <div className={classes.about}>
+      <Title className={classes.title}>What is GoodGrub?</Title>
+
+      <Text>
+        Welcome to GoodGrub, the ultimate recipe app for delicious and easy-to-make meals. Whether you're a beginner cook or a seasoned pro, our app has something for everyone.
+        <br /><br />
+        Our app features a wide variety of recipes, from classic comfort foods to international dishes and healthy options. We make it easy to find the perfect recipe for any occasion, with options sorted by cuisine, ingredients, and dietary restrictions.
+        <br /><br />
+        We believe that cooking should be fun, easy, and accessible to everyone, which is why we're dedicated to providing the best recipe app on the market. So why wait? Download GoodGrub now and start cooking up a storm!
+      </Text>
+    </div>
+  );
+}
