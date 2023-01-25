2654cf37e6af1314269dc55da5bc5cd65fe8ebe8
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-01-25 13:01:52 -0500

    Add category components

---
+++ src/components/category/Categories.js
@@ -0,0 +1,42 @@
+import CategoryItem from './CategoryItem';
+import React from 'react';
+import classes from './Categories.module.scss';
+
+export const Categories = ({
+  categories,
+  categoryError,
+  categoryIsLoading,
+  categoryIsError,
+  selectedCategory,
+  setSelectedCategory,
+  setQuery,
+}) => {
+  if (categoryIsLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (categoryIsError) {
+    return (
+      <div>
+        Error:
+        {categoryError.message}
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.categories__container}>
+      {categories.map((item) => (
+        <CategoryItem
+          category={item}
+          key={item.idCategory}
+          onClickHandler={() => {
+            setSelectedCategory(item.strCategory);
+            setQuery('');
+          }}
+          selectedCategory={selectedCategory}
+        />
+      ))}
+    </div>
+  );
+}
