40e2d5ea37537d4ff5d12cfc6f66ff8030ecfdca
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2023-01-26 20:24:53 -0500

    Fix linked buttons

--- src/components/buttons/LinkedButton.js
+++ src/components/buttons/LinkedButton.js
@@ -4,6 +4,7 @@ import classes from './Button.module.scss';
 export const LinkedButton = ({ children, link = '/', variant = 'secondary' }) => {
   return (
     <a
+      href={link}
       type="button"
       className={`${classes.button} ${classes[`variant__${variant}`]}`}
     >
